5d438e74c5cf604b7644b940fbd1a684ce7cd33d
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-16 22:48:07 +0200

    Add files via upload

---
+++ Migrations/20230304142349_init2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Katalog.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder) //Методът Up създава таблиците на базата данни
+        {
+            migrationBuilder.RenameColumn(
+                name: "Number",
+                table: "AspNetUsers",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder) //Методът Down изтрива таблиците на базата данни
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "AspNetUsers",
+                newName: "Number");
+        }
+    }
+}
