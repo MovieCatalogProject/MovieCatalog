f343e7a9fbd18fa82c32ab6cd307748dd650b060
Kokicha99 <128089304+Kokicha99@users.noreply.github.com>
2023-03-16 23:00:50 +0200

    Add files via upload

---
+++ Repositories/Implement/FileServices.cs
@@ -0,0 +1,69 @@
+﻿using Katalog.Repositories.Abstract;
+
+namespace Katalog.Repositories.Implementation
+{
+    public class FileServices : IFileServices
+    {
+        private readonly IWebHostEnvironment environment;
+        public FileServices(IWebHostEnvironment env)
+        {
+            this.environment = env;
+        }
+
+        public Tuple<int, string> SaveImage(IFormFile imageFile)
+        {
+            try
+            {
+                var wwwPath = this.environment.WebRootPath;
+                var path = Path.Combine(wwwPath, "Uploads");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                // проверява кои разширения са разрешени:
+
+                var ext = Path.GetExtension(imageFile.FileName);
+                var allowedExtensions = new string[] { ".jpg", ".png", ".jpeg" };
+                if (!allowedExtensions.Contains(ext))
+                {
+                    string msg = string.Format("Only {0} extensions are allowed", string.Join(",", allowedExtensions));
+                    return new Tuple<int, string>(0, msg);
+                }
+                string uniqueString = Guid.NewGuid().ToString();
+
+                // създаваме уникално име на файл:
+
+                var newFileName = uniqueString + ext;
+                var fileWithPath = Path.Combine(path, newFileName);
+                var stream = new FileStream(fileWithPath, FileMode.Create);
+                imageFile.CopyTo(stream);
+                stream.Close();
+                return new Tuple<int, string>(1, newFileName);
+            }
+            catch (Exception ex)
+            {
+                return new Tuple<int, string>(0, "Error has occured");
+            }
+        }
+
+        public bool DeleteImage(string imageFileName)
+        {
+            try
+            {
+                var wwwPath = this.environment.WebRootPath;
+                var path = Path.Combine(wwwPath, "Uploads\\", imageFileName);
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
