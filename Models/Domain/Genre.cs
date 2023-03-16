e0304169264421fc69ac0724b0fb2cb309d0ddd0
Kokicha99 <128089304+Kokicha99@users.noreply.github.com>
2023-03-16 22:51:32 +0200

    Add files via upload

---
+++ Models/Domain/Genre.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Katalog.Models.Domain
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Required] //Не може да бъде празно
+        public string GenreName { get; set; }
+    }
+}
