9f54a3ee3d7534098a25e7dd1303c6555c89275c
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-16 22:43:31 +0200

    Add files via upload

---
+++ Controllers/UserAuthenticationController.cs
@@ -0,0 +1,65 @@
+﻿using Katalog.Repositories.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Katalog.Models.DTO;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Katalog.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        private IUserAuthenticationService authService;
+        public UserAuthenticationController(IUserAuthenticationService authService)
+        {
+            this.authService = authService;
+        }
+        //Създаваме потребител с админски права и го коментираме, защото ни трябва само един потребител с такива права.
+
+        //public async Task<IActionResult> Register()
+        //{
+        //    var model = new RegistrationModel
+        //    {
+        //        Email = "admin@gmail.com",
+        //        Username = "Admin",
+        //        Name = "Svilen",
+        //        Password = "Admin@123",
+        //        PasswordConfirm = "Admin@123",
+        //        Role = "Admin",
+        //    };
+
+        //    //    //Ако трябва да се регистрираме като обикновен потребител, да се промени ??Role="User"??
+
+        //    var result = await authService.RegisterAsync(model);
+        //    return Ok(result.Message);
+        //}
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var result = await authService.LoginAsync(model);
+
+            if (result.StatusCode ==1)
+                return RedirectToAction("Index","Home");
+
+            else
+            {
+                TempData["msg"] = "Could no log in...";
+                return RedirectToAction(nameof(Login));
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await authService.LogoutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+
+    }
+}
