39eaa48d0e8475f2848fd7c80469146605f5f856
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-16 22:51:05 +0200

    Update ApplocationUser.cs

--- Models/Domain/ApplocationUser.cs
+++ Models/Domain/ApplocationUser.cs
@@ -1,1 +1,10 @@
 
+using Microsoft.AspNetCore.Identity;
+
+namespace Katalog.Models.Domain
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
