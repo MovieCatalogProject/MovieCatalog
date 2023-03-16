a08090e1dede2ed0f2a05475fa80cbea579ffe90
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-16 22:52:00 +0200

    Add files via upload

---
+++ Models/Domain/Movie.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Build.Framework;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Katalog.Models.Domain
+{
+    public class Movie
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        public string? ReleaseYear { get; set; }
+
+        public string? MovieImage { get; set; }  //запазваме името на снимката с extension (image001.jpg ; ...)
+        [Required]
+        public string? Cast { get; set; }
+        [Required]
+        public string? Director { get; set; }
+
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+        [NotMapped]
+        [Required]
+        public List<int>? Genres { get; set; }
+        [NotMapped]
+        public IEnumerable<SelectListItem>? GenreList { get; set; }
+        [NotMapped]
+        public string? GenreNames { get; set; }
+
+        [NotMapped]
+        public MultiSelectList? MultiGenreList { get; set; }
+    }
+}
---
+++ Models/Domain/MovieGenre.cs
@@ -0,0 +1,9 @@
+﻿namespace Katalog.Models.Domain
+{
+    public class MovieGenre //комбинира Movie и Genre
+    {
+        public int id { get; set; }
+        public int MovieId { get; set; }
+        public int GenreId { get; set; }
+    }
+}
