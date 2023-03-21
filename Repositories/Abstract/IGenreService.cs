a0e3d4ae768a4de58f3971fc2f234666c45ea16e
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-21 19:58:19 +0200

    Update IGenreService.cs

--- Repositories/Abstract/IGenreService.cs
+++ Repositories/Abstract/IGenreService.cs
@@ -1,16 +1,19 @@
-﻿using Katalog.Controllers;
+using Katalog.Controllers;
 using Katalog.Models.Domain;
 using Katalog.Models.DTO;
 
 namespace Katalog.Repositories.Abstract
 {
-    public interface IGenreService
+    public interface IGenreService //Управлява Genre обектите   
     {
         
-        bool Add(Genre model);
-        bool Update(Genre model);
-        Genre GetById(int id);
-        bool Delete(int id);
-        IQueryable<Genre> List();
+        bool Add(Genre model); //Методът връща информация дали операцията относно добавяне на жанр в базата данни е била успешна
+        bool Update(Genre model);//Проверява дали обновлението на даден жанр е било успешно
+        Genre GetById(int id); // Този метод взема "номера" (int),
+        // представляващ уникалния идентификатор на даден Genre обект, и връща съответния Genre обект.
+        bool Delete(int id); //Взима id-то на даден жанр и го изтрива от базата данни
+        IQueryable<Genre> List(); //Извлича списък на всички жанрове от базата данни и връща "IQueryable<Genre>" обект, който може да се 
+        //използва за още заявки към базата данни ??
+        //IQueryable - извлича информация от база данни
     }
 }
