692298bb1cdef2ec6344b8af4ccbba2c7ceabc43
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-21 19:59:18 +0200

    Update IUserAuthenticationService.cs

--- Repositories/Abstract/IUserAuthenticationService.cs
+++ Repositories/Abstract/IUserAuthenticationService.cs
@@ -1,15 +1,16 @@
-﻿using Katalog.Models.DTO;
+using Katalog.Models.DTO;
 
 namespace Katalog.Repositories.Abstract
 {
-    public interface IUserAuthenticationService
+    public interface IUserAuthenticationService 
     {
-        Task<Status> LoginAsync(LoginModel model);
+        Task<Status> LoginAsync(LoginModel model);//Взима LoginModel-а (DTO), който съдържа login информацията на потребителя
+        //и връща Status съобщение, което показва дали "логването" е било успешно.
 
-        Task LogoutAsync();
+        Task LogoutAsync();//Методът изкарва потребителя от профила му
 
-        Task<Status> RegisterAsync(RegistrationModel model);
-        //Task<Status> ChangePasswordAsync(ChangePasswordModel model, string username);
+        Task<Status> RegisterAsync(RegistrationModel model); //Взима RegistrationModel-а (DTO), съдържащ информацията за регистрирането
+        //на потребителя, и връща Status съобщение, което показва дали регистрирането е било успешно
 
     }
 }
