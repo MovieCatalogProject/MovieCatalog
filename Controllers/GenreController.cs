d403089ee9491f27d051868e9487a7af3892cd93
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-21 19:51:05 +0200

    Update GenreController.cs
    Документиране

--- Controllers/GenreController.cs
+++ Controllers/GenreController.cs
@@ -1,4 +1,3 @@
-﻿
 using Katalog.Models.Domain;
 using Katalog.Repositories.Abstract;
 using Microsoft.AspNetCore.Authorization;
@@ -6,23 +5,24 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Katalog.Controllers
 {
-    [Authorize]
+    [Authorize] // Необходимо е удостоверение, за да се получи достъп до методите в контролера 
     public class GenreController : Controller
     {
-        private readonly IGenreService _genreService;
+        private readonly IGenreService _genreService; //IGenreService - интърфейс зададен в "Abstract"
         public GenreController(IGenreService genreService)
         {
             _genreService = genreService;
         }
-        public IActionResult Add()
+        public IActionResult Add() //Get method - използван за показване на "формуляр" на потребителя за създаване на нов жанр.
         {
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Add(Genre model)
+        [HttpPost] //само за POST requests -изпращане на данни
+        public IActionResult Add(Genre model) //Добавя жанр към базата данни : получава създадения обект от първия Add method 
+            // и проверява дали той е валиден чрез ModelState
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid) 
                 return View(model);
             var result = _genreService.Add(model);
             if (result)
@@ -37,14 +37,15 @@ namespace Katalog.Controllers
             }
         }
 
-        public IActionResult Edit(int id)
+        public IActionResult Edit(int id) // Чрез методът редактираме жанр
         {
             var data = _genreService.GetById(id);
             return View(data);
         }
 
         [HttpPost]
-        public IActionResult Update(Genre model)
+        public IActionResult Update(Genre model) //Методът получава ъпдейтнат жанр и се опитва да обнови съответния обект в базата данни.
+            
         {
             if (!ModelState.IsValid)
                 return View(model);
@@ -52,7 +53,7 @@ namespace Katalog.Controllers
             if (result)
             {
                 TempData["msg"] = "Added Successfully";
-                return RedirectToAction(nameof(GenreList));
+                return RedirectToAction(nameof(GenreList)); //След успешно ъпдейтване, потребителят се изпраща обратно в списъка с жанрове
             }
             else
             {
@@ -61,13 +62,13 @@ namespace Katalog.Controllers
             }
         }
 
-        public IActionResult GenreList()
+        public IActionResult GenreList() // връща всички жанрове от базата данни, използвайки "_genreService"
         {
             var data = this._genreService.List().ToList();
             return View(data);
         }
 
-        public IActionResult Delete(int id)
+        public IActionResult Delete(int id) //Изтрива жанрове от базата данни
         {
             var result = _genreService.Delete(id);
             return RedirectToAction(nameof(GenreList));
