226f7aae1668a6025a73cdffa950f09e9e56f152
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-21 19:51:35 +0200

    Update HomeController.cs
    Документиране

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,30 @@
-﻿using Katalog.Repositories.Abstract;
+using Katalog.Repositories.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Katalog.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IMovieService _movieService;
+        private readonly IMovieService _movieService; //IMovieService - интърфейс от "Abstract"
         public HomeController(IMovieService movieService)
         {
             _movieService = movieService;
         }
-        public IActionResult Index(string term = "", int currentPage = 1)
+        public IActionResult Index(string term = "", int currentPage = 1) //Методът се вика, когато потребителят е в "home page-a". 
+            //Използа интърфейса "IMovieservice", за да получи списък с филми въз основа на въведената дума за търсене (term) и номера
+            // на текущата страница (currentPage).
         {
             var movies = _movieService.List(term, true, currentPage);
-            return View(movies);
+            return View(movies); //Извежда се филма
         }
 
-        public IActionResult About()
+        public IActionResult About() //About страницата на сайта.
         {
             return View();
         }
 
-        public IActionResult MovieDetail(int movieId)
+        public IActionResult MovieDetail(int movieId) //Методът се вика, когато потребителят натисне върху филм, за да види подробностите му.
+            //Използва параметърът movieId и IMovieService интерфейса, за да вземе детайлите/подробностите на филма със съответното id.
         {
             var movie = _movieService.GetById(movieId);
             return View(movie);
