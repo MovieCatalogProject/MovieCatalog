8eebc7599156bcaddc41d43af99aeb5bbf62f914
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-21 19:57:10 +0200

    Update MovieListVm.cs

--- Models/DTO/MovieListVm.cs
+++ Models/DTO/MovieListVm.cs
@@ -1,13 +1,14 @@
-﻿using Katalog.Models.Domain;
+using Katalog.Models.Domain;
 
 namespace Katalog.Models.DTO
 {
     public class MovieListVm
     {
-        public IQueryable<Movie> MovieList { get; set; }
-        public int PageSize { get; set; }
-        public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
-        public string? Term { get; set; }
+        public IQueryable<Movie> MovieList { get; set; }//IQueryable интърфейса позволява отлагането на изпълнението на заявки??
+        //MovieList - списъка с филми, които искаме да покажем/представим в web апликацията
+        public int PageSize { get; set; } //Свойство, казващо колко филма да има на една страница (5 в този случай)
+        public int CurrentPage { get; set; }//Показва текущата страница, в която се намира потребителят
+        public int TotalPages { get; set; }//Показва общия брой на страниците
+        public string? Term { get; set; }//Ключова дума, която потребителят въвежда, за да филтрира филмите
     }
 }
