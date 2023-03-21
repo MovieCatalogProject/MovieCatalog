bc48c5430ff6d21fd5f661c7d5ecad617a0b44c6
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-21 19:58:05 +0200

    Update IFileServices.cs

--- Repositories/Abstract/IFileServices.cs
+++ Repositories/Abstract/IFileServices.cs
@@ -1,8 +1,9 @@
-﻿namespace Katalog.Repositories.Abstract
+namespace Katalog.Repositories.Abstract
 {
     public interface IFileServices
     {
-        public Tuple<int, string> SaveImage(IFormFile imageFile);
-        public bool DeleteImage(string imageFileName);
+        public Tuple<int, string> SaveImage(IFormFile imageFile);// Tuple има две стойности : 1. int - представлява статус код, който
+        //информира дали операцията е била успешна или не; 2. string - предоставя допълнителна информация относно операцията (съобщение)
+        public bool DeleteImage(string imageFileName); //Методът казва дали снимката е била успешно изтрита
     }
 }
