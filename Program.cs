56ef1ddf518f59ae1032c15e0d17fa7d82af03ba
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-21 20:01:46 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,15 +6,14 @@ using Katalog.Repositories.Implementation;
 using Katalog.Repositories.Implement;
 using Katalog.Repositories.Implementation;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); //използва се за конфигуриране и изграждане на уеб приложението.
 
-// 
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IUserAuthenticationService, UserAuthenticationService>();
-builder.Services.AddScoped<IGenreService, GenreService>();
-builder.Services.AddScoped<IFileServices, FileServices>();
-builder.Services.AddScoped<IMovieService, MovieService>();
+builder.Services.AddScoped<IUserAuthenticationService, UserAuthenticationService>(); //Позволява на уеб приложението да удостоверява и упълномощава потребителите.
+builder.Services.AddScoped<IGenreService, GenreService>(); //Позволява на уеб приложението да извършва свързани с жанра операции върху филми.
+builder.Services.AddScoped<IFileServices, FileServices>(); //Позволява на уеб приложението да извършва операции, свързани с файлове - качване на файлове.
+builder.Services.AddScoped<IMovieService, MovieService>(); //Позволява на уеб приложението да извършва операции, свързани с филми - добавяне, ъпдейтване и изтриване на филми.
 
 
 builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("conn"))); //SQL connection string - "conn"
