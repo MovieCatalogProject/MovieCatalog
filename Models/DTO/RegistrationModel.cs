e9deb0442af500bb8071e2f67c1db54f1654a78d
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-21 19:57:27 +0200

    Update RegistrationModel.cs

--- Models/DTO/RegistrationModel.cs
+++ Models/DTO/RegistrationModel.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace Katalog.Models.DTO
 {
-    public class RegistrationModel
+    public class RegistrationModel //Клас, играещ ролята на модел за регистрация на даден потребител
     {
         [Required]
         public string Name { get; set; }
@@ -13,6 +14,13 @@ namespace Katalog.Models.DTO
 
         [Required]
         [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*[#$^+=!*()@%&]).{6,}$", ErrorMessage = "Minimum length 6 and must contain  1 Uppercase,1 lowercase, 1 special character and 1 digit")]
+        //'^' - началото на низа
+        //'(?=.*?[A-Z])' - изисква поне една главна буква
+        //'(?=.*?[a-z])' - изисква поне една малка буква
+        //'(?=.*?[0-9])' - изисква поне една цифра
+        //'(?=.*[#$^+=!*()@%&])' - изисква поне един специален знак
+        //'.{ 6,}' - трябва паролата да е поне 6 знака дълга
+        //'$' - края на низа
         public string Password { get; set; }
         [Required]
         [Compare("Password")]
