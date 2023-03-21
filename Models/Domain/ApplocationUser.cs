b12ccb7f42fdf6ed0324504b17718e7590e30184
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-21 19:54:50 +0200

    Update ApplocationUser.cs

--- Models/Domain/ApplocationUser.cs
+++ Models/Domain/ApplocationUser.cs
@@ -1,10 +1,10 @@
-
 using Microsoft.AspNetCore.Identity;
 
 namespace Katalog.Models.Domain
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser // Класа се използва за "представяне" на потребител 
+        //IdentityUser - вграден клас в ASP.NET, който снабдява с основни данни за потребителя (Id, Email, PasswordHarsh)
     {
-        public string Name { get; set; }
+        public string Name { get; set; } //Name е в допълнение към IdentityUser данните
     }
 }
