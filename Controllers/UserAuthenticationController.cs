5d33e1d4ecef4b40d74d8ba89e8b178ac349a0f4
siskooo <98637261+siskooo@users.noreply.github.com>
2023-03-21 19:53:26 +0200

    Update UserAuthenticationController.cs

--- Controllers/UserAuthenticationController.cs
+++ Controllers/UserAuthenticationController.cs
@@ -1,4 +1,4 @@
-﻿using Katalog.Repositories.Abstract;
+using Katalog.Repositories.Abstract;
 using Microsoft.AspNetCore.Mvc;
 using Katalog.Models.DTO;
 using System.ComponentModel.DataAnnotations;
@@ -13,6 +13,7 @@ namespace Katalog.Controllers
         {
             this.authService = authService;
         }
+        
         //Създаваме потребител с админски права и го коментираме, защото ни трябва само един потребител с такива права.
 
         //public async Task<IActionResult> Register()
@@ -27,18 +28,19 @@ namespace Katalog.Controllers
         //        Role = "Admin",
         //    };
 
-        //    //    //Ако трябва да се регистрираме като обикновен потребител, да се промени ??Role="User"??
+        //    //Ако трябва да се регистрираме като обикновен потребител, да се промени ??Role="User"??
 
         //    var result = await authService.RegisterAsync(model);
         //    return Ok(result.Message);
         //}
 
-        public async Task<IActionResult> Login()
+        public async Task<IActionResult> Login()//връща празно view, където потребителят си въвежда данните
         {
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> Login(LoginModel model)
+        public async Task<IActionResult> Login(LoginModel model) //Методът взима въведените от потребителя данни, проверява дали са
+            //валидни и ако да го изпраща в home страницата
         {
             if (!ModelState.IsValid)
                 return View(model);
@@ -50,12 +52,12 @@ namespace Katalog.Controllers
 
             else
             {
-                TempData["msg"] = "Could no log in...";
+                TempData["msg"] = "Could not log in...";
                 return RedirectToAction(nameof(Login));
             }
         }
 
-        public async Task<IActionResult> Logout()
+        public async Task<IActionResult> Logout() //Изкарва потребителя от сайта и го изпраща в login страницата
         {
             await authService.LogoutAsync();
             return RedirectToAction(nameof(Login));
